e289c10b67f7447c9a9563f40abd4f0a8dd66fe4
BraySty <123750122+BraySty@users.noreply.github.com>
2024-03-20 09:36:46 +0000

    Update app.java

--- src/Main/app.java
+++ src/Main/app.java
@@ -5,10 +5,17 @@ import Clases.UsusarioImplOracle;
 public class app {
 
 	public static void main(String[] args) {
-		// Injeccion de dependencias
-		UsusarioFactory obj = new UsusarioFactory(new UsusarioImplOracle());
-		obj.getInsertaUsusario();
-		obj.getEliminaUsuarioByID(1);
+		
+												// Injeccion de dependencias
+		UsusarioFactory obj1 = new UsusarioFactory(new UsusarioImplOracle());
+		obj1.getInsertaUsusario();
+		obj1.getEliminaUsuarioByID(1);
+		
+												// Injeccion de dependencias
+		UsusarioFactory obj2 = new UsusarioFactory(new UsusarioImplMongo());
+		obj2.getInsertaUsusario();
+		obj2.getEliminaUsuarioByID(1);
+		
 	}
 	
 }
